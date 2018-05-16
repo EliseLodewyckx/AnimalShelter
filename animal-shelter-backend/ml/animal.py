e4de09a4204df6e631aae06c369a4566e5ad7418
Elise Lodewyckx <elise.lodewyckx@cegeka.com>
2018-05-16 14:59:03 +0200

    Elise, Gilliam: added "mix" boolean to animal

--- animal-shelter-backend/ml/animal.py
+++ animal-shelter-backend/ml/animal.py
@@ -1,6 +1,7 @@
 class Animal(object):
-    def __init__(self, animalType, sex, intact, age):
+    def __init__(self, animalType, sex, intact, age, mix):
         self.animalType = animalType
         self.sex = sex
         self.intact = intact
         self.age = age
+        self.mix = mix
